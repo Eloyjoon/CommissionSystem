9a8e0fe70192fce089f64e60a494152bd1e8f6c8
eloyjoon <alireza.sabouei@gmail.com>
2021-10-08 14:08:37 +0330

    Added Kendo,Sidebar,FOntAwesome

--- CommisionSystem.WebApplication/Startup.cs
+++ CommisionSystem.WebApplication/Startup.cs
@@ -35,6 +35,8 @@ namespace CommisionSystem.WebApplication
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
